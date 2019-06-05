be8cd95e9b928aa990bc541b1dafc7d8a40f5443
hooper <hooper82@gmail.com>
2019-06-05 12:22:28 +1000

    adding traffic to main.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from seven import Seven
 from matrix import Matrix
 from keypad import Keypad
+from traffic import TrafficLights
 
 from time import sleep
 
@@ -8,6 +9,7 @@ from time import sleep
 if __name__ == "__main__":
     seven = Seven()
     matrix = Matrix()
+    traffic = TrafficLights()
     keypad = Keypad(seven.add)
 
     while True:
