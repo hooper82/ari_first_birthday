b21f97ba636149b077d6dbaf0774bb806cfa26a2
hooper <hooper82@gmail.com>
2019-06-05 11:29:08 +1000

    Changing the way the seven add digit logic works.

--- seven.py
+++ seven.py
@@ -5,13 +5,22 @@ from luma.core.virtual import sevensegment
 
 class Seven(object):
     def __init__(self):
+        print("Initializing Seven...", end='')
         self._serial = spi(port=1, device=0, gpio=noop())
         self._max = max7219(self._serial)
         self._device = sevensegment(self._max)
+        self._buffer = ''
+        print("...Done!")
 
     def add(self, digit):
         print(digit)
         if digit in ['*', '#']:
-            self._device.text = ''
+            self._buffer = ''
         else:
-            self._device.text += digit
+            self._buffer = '{}{}'.format(digit, self._buffer)
+            if len(self._buffer) > 8:
+                self._buffer = self._buffer[:-1]
+        self._update()
+
+    def _update(self):
+        self._device.text = self._buffer
