e3a90c1c865fd9d92d4b9a3bf7766b84f55b9ed2
hooper <hooper82@gmail.com>
2019-06-05 12:21:13 +1000

    Adding logging (print)

--- traffic.py
+++ traffic.py
@@ -3,17 +3,21 @@ from gpiozero import LED
 
 class TrafficLights(object):
 	def __init__(self):
+		print("Initializing Traffic Lights...", end='')
 		self._green = LED(13)
 		self._orange = LED(6)
 		self._red = LED(5)
+		print("...Done!")
 
 		self._test()
 
 	def _test(self):
+		print('Testing Traffic Lights...', end='')
 		for l in [self._green, self._orange, self._red]:
 			l.on()
 			sleep(0.1)
 			l.off()
+		print('...Done!')
 
 	def cycle(self):
 		pass
