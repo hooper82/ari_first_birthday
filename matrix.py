a6fe0a361c92b8f4f6ddf333fedfc40619eee044
hooper <hooper82@gmail.com>
2019-06-05 12:35:50 +1000

    Fixing typo

--- matrix.py
+++ matrix.py
@@ -45,7 +45,7 @@ class Matrix(object):
         print('changing image')
         image_names = list(IMAGES.keys())
         if self._current_image_name in image_names:
-            image_names.pop(self._current_image)
+            image_names.pop(self._current_image_name)
 
         self._current_image_name = choice(image_names)
         self._draw_image(self._current_image_name)
